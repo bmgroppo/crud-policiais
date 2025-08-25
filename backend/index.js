5bf738aca90836fa073a14e7ca1bd3a387fb67d9
Bruno Groppo <bmgroppo@gmail.com>
2025-08-25 10:48:49 -0300

    código semi-pronto. Agora vou entender o bcrypt e validador de cpf

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const routes = require('./src/routes/crud_policiaisRoutes');
 
 app.use(express.json());
 app.use(cors());
-app.use(routes);
+app.use('/api', routes); // Adiciona um prefixo /api a todas as rotas
 
 const PORT = 3000
 app.listen(PORT, () => {

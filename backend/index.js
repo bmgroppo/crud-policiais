3ccd422a4f19fc0e4b99a65a608830f8c988677d
Bruno Groppo <bmgroppo@gmail.com>
2025-08-25 10:33:02 -0300

    criado o index.js na pasta raiz com express e cors

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,13 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const routes = require('./src/routes/crud_policiaisRoutes');
 
+app.use(express.json());
+app.use(cors());
+app.use(routes);
+
+const PORT = 3000
+app.listen(PORT, () => {
+    console.log(`Servidor rodando no endereço http://localhost:${PORT}`);
+});

7f52fadb25910f221db258344c3e5c9c93792422
Bruno Groppo <bmgroppo@gmail.com>
2025-08-25 10:08:12 -0300

    Criados as pastas para organizar no modelo MVC. CJá criado o repositório

---
+++ backend/src/repository/crud_policiaisRepository.js
@@ -0,0 +1,19 @@
+const db = require('../config/db');
+exports.buscarTodosPoliciais = (retorno) => {
+    db.query('SELECT * FROM policiais', retorno);
+};
+exports.buscarPoliciaisPorId = (id, retorno) => {
+    db.query('SELECT * FROM policiais WHERE id = ?', [id], retorno);
+};
+
+exports.inserirPolicial = (policiais, retorno) => {
+    db.query('INSERT INTO policiais (RG_civil, RG_militar, CPF, data_nascimento, matricula) VALUES (?, ?, ?, ?, ?)', [policiais.RG_civil, policiais.RG_militar, policiais.CPF, policiais.data_nascimento, policiais.matricula], retorno);
+};
+
+exports.atualizarPolicial = (id, policiais, retorno) => {
+    db.query('UPDATE policiais SET RG_civil = ?, RG_militar = ?, CPF = ?, data_nascimento = ?, matricula = ? WHERE id = ?', [policiais.RG_civil, policiais.RG_militar, policiais.CPF, policiais.data_nascimento, policiais.matricula, id], retorno);
+};
+
+exports.excluirPolicial = (id, retorno) => {
+    db.query('DELETE FROM policiais WHERE id = ?', [id], retorno);
+};
